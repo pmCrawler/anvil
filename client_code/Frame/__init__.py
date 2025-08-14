25836fa3ce611b2d01b82087e1448fae19ce3364
helmanan@gmail.com <helmanan@gmail.com>
2025-08-14 19:13:16 +0000

    Edited Frame

--- client_code/Frame/__init__.py
+++ client_code/Frame/__init__.py
@@ -11,5 +11,10 @@ class Frame(FrameTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        
+
         # Any code you write here will run before the form opens.
+
+    def nav_link_events_click(self, **event_args):
+        """This method is called when the component is clicked"""
+        open_form("EventList")
+        pass
