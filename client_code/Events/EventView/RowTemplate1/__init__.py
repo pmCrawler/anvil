d8489a5f85ff6d0c7787f73df9bc134ddefebd8c
helmanan@gmail.com <helmanan@gmail.com>
2025-09-10 12:46:38 +0000

    Edited settings, Events.EventView.RowTemplate1 and Events.EventView; Deleted Events.EventView.RowTemplate2, Events.EventView.RowTemplate4, Events.EventView.RowTemplate3 and Events.EventView.ItemTemplate2

--- client_code/Events/EventView/RowTemplate1/__init__.py
+++ client_code/Events/EventView/RowTemplate1/__init__.py
@@ -6,11 +6,10 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime
 
 
 class RowTemplate1(RowTemplate1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
-        # Any code you write here will run before the form opens.
