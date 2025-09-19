c927862380f83bf3687adbcda13863e7d5915c0b
helmanan@gmail.com <helmanan@gmail.com>
2025-09-19 12:53:07 +0000

    Edited Tasks.TaskForm

--- client_code/Tasks/TaskForm/__init__.py
+++ client_code/Tasks/TaskForm/__init__.py
@@ -13,13 +13,13 @@ class TaskForm(TaskFormTemplate):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        self.event = event
+        self.events = []
         if not event:
             self.events = anvil.server.call("get_events", user_id=None)
-            self.dd_event_list.items = [ev["title"] for ev in self.events]
         else:
-            self.dd_event_list.item = event["title"]
+            self.events.append(event)
         # self.refresh_comment_panel_items()
+        self.dd_event_list.items = [ev["title"] for ev in self.events]
         pass
 
     # def refresh_comment_panel_items(self):
@@ -28,19 +28,17 @@ class TaskForm(TaskFormTemplate):
     def btn_add_task_click(self, **event_args):
         """This method is called when the component is clicked."""
 
-        # ev_selected = [
-        #     ev for ev in self.events if ev["title"] == self.dd_event_list.selected_value
-        # ]
-        event_link = self.event
-        task = app_tables.tasks.add_row(
-            event_link=event_link,  # self.dd_event_list.selected_value,
+        selected_event = [
+            ev for ev in self.events if ev["title"] == self.dd_event_list.selected_value
+        ]
+        app_tables.tasks.add_row(
+            event_link=selected_event,
             task=self.txtbx_title.text,
             details=self.txtarea_details.text,
             due_date=self.dtpkr_due_date.date,
             duration=self.txtbx_duration.text,
             status=self.rgpnl_status.selected_value,
-        ).get_id()
-        print(f"""Task Id: {task}""")
+        )
 
         open_form(get_open_form())
         # anvil.server.call("save_task", event_args)
