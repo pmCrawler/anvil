7d79a303b168ebdc8f96c53db3c85cbbd5adf6f7
helmanan@gmail.com <helmanan@gmail.com>
2025-12-08 14:07:43 +0000

    Edited Events.EventForm

--- client_code/Events/EventForm/__init__.py
+++ client_code/Events/EventForm/__init__.py
@@ -27,7 +27,7 @@ class EventForm(EventFormTemplate):
 
     def btn_start_click(self, **event_args):
         """This method is called when the component is clicked."""
-        result = anvil.server.call("event_ai_test")
+        result = anvil.server.call("test_connection")
         print(result)
 
         self.user_input = self.get_user_input()
