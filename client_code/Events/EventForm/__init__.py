6377dfdfa2391f4227edf65788f8d227e5474fe6
helmanan@gmail.com <helmanan@gmail.com>
2025-12-09 19:43:45 +0000

    Edited Events.EventForm

--- client_code/Events/EventForm/__init__.py
+++ client_code/Events/EventForm/__init__.py
@@ -10,12 +10,6 @@ from ... import Events
 import anvil.http
 import json
 
-# import asyncio
-
-
-QUESTION_WF_URL = "http://localhost:5678/webhook/69aee61f-e514-417d-ad16-0615b6e1a9c9"
-EVENT_WF_URL = "http://localhost:5678/webhook/dafb4274-ddf0-4874-a0e1-5a362c525170"
-
 
 class EventForm(EventFormTemplate):
     def __init__(self, **properties):
@@ -26,68 +20,6 @@ class EventForm(EventFormTemplate):
         self.user_input = dict()
 
         self._load_default_input()
-
-    def button_search_address_click(self, **event_args):
-        """Search for address using server-side geocoding"""
-        address_query = self.text_box_address.text
-
-        if not address_query:
-            alert("Please enter an address")
-            return
-
-        # Show loading
-        with anvil.server.no_loading_indicator:
-            Notification("Searching for address...", timeout=2).show()
-
-        # Call server
-        results = anvil.server.call("geocode_address", address_query)
-
-        if results["success"]:
-            # Show results in dropdown
-            self.show_address_results(results["addresses"])
-        else:
-            alert(f"Error: {results['error']}")
-
-    def show_address_results(self, addresses):
-        """Display address options to user"""
-        # Show results in repeating panel or dropdown
-        self.dropdown_address_results.items = [
-            addr["formatted_address"] for addr in addresses
-        ]
-        self.dropdown_address_results.visible = True
-
-    def dropdown_address_results_change(self, **event_args):
-        """User selected an address"""
-        selected = self.dropdown_address_results.selected_value
-
-        if selected:
-            self.location_data = selected
-            self.label_selected_address.text = selected["formatted_address"]
-            self.button_save.enabled = True
-
-    def address_selected(self, address_data):
-        """Called when user selects address from autocomplete"""
-        print(f"Address selected: {address_data}")
-
-        # Store address data
-        self.location_data = {
-            "formatted_address": address_data["formatted_address"],
-            "latitude": address_data["latitude"],
-            "longitude": address_data["longitude"],
-            "city": address_data["components"].get("city"),
-            "region": address_data["components"].get("region"),
-            "region_code": address_data["components"].get("region_code"),
-            "postal_code": address_data["components"].get("postal_code"),
-            "country": address_data["components"].get("country"),
-            "country_code": address_data["components"].get("country_code"),
-        }
-
-        # Display formatted address
-        self.text_box_address.text = address_data["formatted_address"]
-
-        # Optionally show details
-        self.label_city.text = f"City: {self.location_data.get('city', 'N/A')}"
-        self.label_country.text = f"Country: {self.location_data.get('country', 'N/A')}"
 
     def _load_default_input(self):
         self.title.text = "Liverpool vs West Ham Football Match Viewing Party"
@@ -108,11 +40,11 @@ class EventForm(EventFormTemplate):
             "food_bev": True if self.switch_food.selected else False,
             "event_setting": self.rgp_setting.selected_value,
         }
-        # return self.user_input
 
     def btn_start_click(self, **event_args):
         """This method is called when the component is clicked."""
 
+        result = anvil.server.call("")
         self.btn_start.visible = False
         self.get_user_input()
         self.resp = anvil.server.call("run_event_ai", self.user_input)
