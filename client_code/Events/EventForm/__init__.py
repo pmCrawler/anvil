494fed87fe898392d85f333602b664ac90502fa3
helmanan@gmail.com <helmanan@gmail.com>
2025-12-09 16:49:42 +0000

    Edited Events.EventForm

--- client_code/Events/EventForm/__init__.py
+++ client_code/Events/EventForm/__init__.py
@@ -25,9 +25,92 @@ class EventForm(EventFormTemplate):
         self.cpanel_options.visible = False
         self.user_input = dict()
 
-        event_schema = anvil.server.call("get_event", "table")
-        print(event_schema)
         self._load_default_input()
+        # self.setup_address_autocomplete()
+
+        location = anvil.server.call("get_user_location_details")
+        ip_info = anvil.server.call("get_ip_info")
+        print(ip_info)
+        print(location)
+
+    def setup_address_autocomplete(self):
+        """Setup Google Places autocomplete on address field"""
+
+        # Add Google Places script (add to native libraries in Anvil)
+        # Or use Anvil's GoogleMap component
+
+        # JavaScript to setup autocomplete
+        js_code = """
+        // Initialize Google Places Autocomplete
+        var input = document.getElementById('address-input');
+        var autocomplete = new google.maps.places.Autocomplete(input, {
+            types: ['address'],  // Only addresses
+            fields: ['address_components', 'formatted_address', 'geometry']
+        });
+        
+        // When user selects an address
+        autocomplete.addListener('place_changed', function() {
+            var place = autocomplete.getPlace();
+            
+            // Extract address components
+            var addressData = {
+                formatted_address: place.formatted_address,
+                latitude: place.geometry.location.lat(),
+                longitude: place.geometry.location.lng(),
+                components: {}
+            };
+            
+            // Parse address components
+            place.address_components.forEach(function(component) {
+                var types = component.types;
+                if (types.includes('street_number')) {
+                    addressData.components.street_number = component.long_name;
+                } else if (types.includes('route')) {
+                    addressData.components.street = component.long_name;
+                } else if (types.includes('locality')) {
+                    addressData.components.city = component.long_name;
+                } else if (types.includes('administrative_area_level_1')) {
+                    addressData.components.region = component.long_name;
+                    addressData.components.region_code = component.short_name;
+                } else if (types.includes('postal_code')) {
+                    addressData.components.postal_code = component.long_name;
+                } else if (types.includes('country')) {
+                    addressData.components.country = component.long_name;
+                    addressData.components.country_code = component.short_name;
+                }
+            });
+            
+            // Send to Anvil Python
+            window.anvilComponent.call('address_selected', addressData);
+        });
+        """
+
+        # Execute JavaScript
+        anvil.js.call_js("eval", js_code)
+
+    def address_selected(self, address_data):
+        """Called when user selects address from autocomplete"""
+        print(f"Address selected: {address_data}")
+
+        # Store address data
+        self.location_data = {
+            "formatted_address": address_data["formatted_address"],
+            "latitude": address_data["latitude"],
+            "longitude": address_data["longitude"],
+            "city": address_data["components"].get("city"),
+            "region": address_data["components"].get("region"),
+            "region_code": address_data["components"].get("region_code"),
+            "postal_code": address_data["components"].get("postal_code"),
+            "country": address_data["components"].get("country"),
+            "country_code": address_data["components"].get("country_code"),
+        }
+
+        # Display formatted address
+        self.text_box_address.text = address_data["formatted_address"]
+
+        # Optionally show details
+        self.label_city.text = f"City: {self.location_data.get('city', 'N/A')}"
+        self.label_country.text = f"Country: {self.location_data.get('country', 'N/A')}"
 
     def _load_default_input(self):
         self.title.text = "Liverpool vs West Ham Football Match Viewing Party"
